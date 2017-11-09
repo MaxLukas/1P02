4bc5a9e1d805ec358e97951dfdae8d1c6de801e7
Max <31972327+MaxLukas@users.noreply.github.com>
2017-11-09 17:18:55 -0500

    actual lab 9

--- lab9/ex2/scripts.js
+++ lab9/ex2/scripts.js
@@ -6,9 +6,9 @@ document.getElementById("red").style.color="red";
 //A function to change colours of elements when called
 function mix(){
 
-	var first = "#0000FF";
-	var second = "#008000";
-	var third = "#FF0000";
+	var first = "#00BB00";
+	var second = "#BB9320";
+	var third = "#AB00FF";
 
 	document.getElementById("blue").style.color=first;
 	document.getElementById("green").style.color=second;
